1e9994aec18da8d4e761b2a4c8f3d368347f029c
yongwook <60510921+CYY1007@users.noreply.github.com>
2022-04-20 00:44:45 +0900

    Add files via upload
    위치하고있는 위치정보를 얻기위해 expo에서 제공하는 API를 활용해서 연습

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
+import * as Location from "expo-location";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,ScrollView, Dimensions} from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-web';
+import { useEffect, useState } from 'react';
 
 const {height:SCREEN_HEIGHIT, width:SCREEN_WIDTH} = Dimensions.get("window");
-
 export default function App() {
+  const [city,Setcity] = useState("Loading...");
+  const [location,setLocation] = useState();
+  const [ok,setOK] = useState(true);
+  const ask = async () =>{
+    let location = null;
+    const {granted:permissioin} = await Location.requestForegroundPermissionsAsync();
+    console.log(permissioin);
+    if(!permissioin){
+      setOK(false);
+    }
+    else{
+      const {coords: {latitude,longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+       location= await Location.reverseGeocodeAsync({latitude,longitude},
+        {useGoogleMaps: false});
+        Setcity(location[0].city)
+      }
+  }
+  useEffect(()=>{
+    ask();
+  },[])
   return (
     <View style={styles.container}>
       <View style={styles.location}>
-        <Text style={styles.cityname}>Location!</Text>
+        <Text style={styles.cityname}>{city}</Text>
       </View>
       <ScrollView horizontal
       showsHorizontalScrollIndicator={false}
-       pagingEnabled
+      pagingEnabled
        contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
